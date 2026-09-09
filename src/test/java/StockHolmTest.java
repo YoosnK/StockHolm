bc23b96a8761204033a03be9c3f8eea1b6c03ac7
YoosnK <yoosnmc@gmail.com>
2026-09-09 13:31:31 +0800

    chore: added test directory and dummy test script

---
+++ src/test/java/StockHolmTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StockHolmTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
